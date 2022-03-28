6c02c2a1d06dc2ca94be2216e393a5a59eceba72
rydev99 <100022705+rydev99@users.noreply.github.com>
2022-03-29 02:41:07 +0700

    update _sendDb.js

--- plugins/_sendDb.js
+++ plugins/_sendDb.js
@@ -1,9 +1,25 @@
-import fs from 'fs'
-let handler = async(m, { conn, text }) => {
-m.reply(user)
+import fetch from 'node-fetch'
+import { format } from 'util'
+let handler = async (m, { text }) => {
+    if (!/^https?:\/\//.test(text)) throw 'Awali *URL* dengan http:// atau https://'
+    let _url = new URL(text)
+    let url = global.API(_url.origin, _url.pathname, Object.fromEntries(_url.searchParams.entries()), 'APIKEY')
+    let res = await fetch("https://json-server.rynz01.repl.co/")
+    if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
+        // delete res
+        throw `Content-Length: ${res.headers.get('content-length')}`
+    }
+    if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m)
+    let txt = await res.buffer()
+    try {
+        txt = format(JSON.parse(txt + ''))
+    } catch (e) {
+        txt = txt + ''
+    } finally {
+        m.reply(txt.slice(0, 65536) + '')
+    }
 }
 
 handler.command = /db/i
-handler.owner = true
-handler.limit = false
+
 export default handler
