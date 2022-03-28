35927d543d81c1b962468bfea3d72409215d275a
rydev99 <100022705+rydev99@users.noreply.github.com>
2022-03-29 03:01:09 +0700

    update autorespon.js

--- plugins/autorespon.js
+++ plugins/autorespon.js
@@ -3,19 +3,12 @@ let handler = m => m
 
 handler.all = async function (m, { conn }) {
 let set = db.data.settings[this.user.jid]
-
-if (set.backup) {
-        if (new Date() * 1 - set.backupTime > 1000 * 60 * 60) {
-            let d = new Date
-            let date = d.toLocaleDateString('id', {
-                day: 'numeric',
-                month: 'long',
-                year: 'numeric'
-            })
-            await global.db.write()
-            this.reply(global.owner[0] + '@s.whatsapp.net', `Database: ${date}`, null)
-            this.sendFile('6283856085455@s.whatsapp.net', fs.readFileSync('./database.json'), 'database.json', '', 0, 0, { mimetype: 'application/json' })
-            set.backupTime = new Date() * 1
+if (set.autoupdatestatus) {
+        if (new Date() * 1 - set.status > 1000) {
+            let _uptime = process.uptime() * 1000
+            let uptime = conn.clockString(_uptime)
+            await m.reply("oke")
+            set.status = new Date() * 1
         }
     }
  
