a7911676225cf3b6dd6af027a5390fe45acb94b5
rydev99 <100022705+rydev99@users.noreply.github.com>
2022-03-29 02:38:28 +0700

    update _sendDb.js

--- plugins/_sendDb.js
+++ plugins/_sendDb.js
@@ -1,7 +1,5 @@
 import fs from 'fs'
 let handler = async(m, { conn, text }) => {
-let user = global.db.data
-conn.sendFile(global.owner[0] + '@s.whatsapp.net', fs.readFileSync('./session.data.json'), 'database.json', '', 0, 0, { mimetype: 'application/json' })
 m.reply(user)
 }
 
