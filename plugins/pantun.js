d160cad19d087e9a038e9dcb0ae64497bbea871f
rydev99 <100022705+rydev99@users.noreply.github.com>
2022-03-28 08:32:14 +0700

    update pantun.js

--- plugins/pantun.js
+++ plugins/pantun.js
@@ -2,20 +2,12 @@ let handler  = async (m, { conn }) => {
   conn.reply(m.chat,`“${pickRandom(global.pantun)}”`, m)
 }
 handler.help = ['pantun']
-handler.tags = ['quotes']
+handler.tags = ['fun']
 handler.command = /^(pantun)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
 
 handler.fail = null
 
-module.exports = handler
+export default handler
 
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
