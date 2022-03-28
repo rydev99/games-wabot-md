c55bf78e31483762574b87305987fb824030a79f
rydev99 <100022705+rydev99@users.noreply.github.com>
2022-03-29 02:02:40 +0700

    update _sendDb.js

--- plugins/_sendDb.js
+++ plugins/_sendDb.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, text }) => {
 conn.sendFile(m.chat, "https://json-server.rynz01.repl.co/", "db.json", null, m)
 }
 
-handler.command = /^['db']$/i
+handler.command = /^('db')$/i
 handler.owner = true
 handler.limit = false
 export default handler
