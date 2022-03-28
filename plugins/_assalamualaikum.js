60a05cbfc0f066a4196971eb43027329dc415308
rydev99 <100022705+rydev99@users.noreply.github.com>
2022-03-28 09:01:58 +0700

    update _assalamualaikum.js

--- plugins/_assalamualaikum.js
+++ plugins/_assalamualaikum.js
@@ -5,10 +5,7 @@ let caption = `*Waalaikummussalam warahmatullahi wabarokatuh*
 _📚 Baca yang dibawah ya!_
 "Orang yang mengucapkan salam seperti ini maka ia mendapatkan 30 pahala, kemudian, orang yang dihadapan atau mendengarnya membalas dengan kalimat yang sama yaitu “Wa'alaikum salam warahmatullahi wabarakatuh” atau ditambah dengan yang lain (waridhwaana). Artinya selain daripada do'a selamat juga meminta pada Allah SWT"
 `
-
-conn.sendButton(m.chat, caption, author, null, [
-        ['Waalaikumsalam', 'Waalaikumsalam'],
-    ], { quoted: m })
+conn.reply(m.chat, caption, m)
 }
 handler.customPrefix = /^(assalamualaikum|salam)/i
 handler.command = new RegExp
