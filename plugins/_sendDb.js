e0f5895c402d0fded54fcbc6e671a3621c5e61a8
rydev99 <100022705+rydev99@users.noreply.github.com>
2022-03-29 02:10:22 +0700

    update _sendDb.js

--- plugins/_sendDb.js
+++ plugins/_sendDb.js
@@ -1,9 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async(m, { conn, text }) => {
 let res = fetch("https://json-server.rynz01.repl.co/")
-if (!res.ok) throw await res.text()
-  let json = await res.json()
-  if(!json.result[0]) throw json
+let json = await res.json()
 m.reply(json)
 }
 
