cb193032e35bdee996d00fd928ac2c2591324d6b
rydev99 <100022705+rydev99@users.noreply.github.com>
2022-03-28 13:24:36 +0700

    update yt-comment.js

--- plugins/yt-comment.js
+++ plugins/yt-comment.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
   if (!text) throw 'No Text'
   conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/youtube-comment', {
-    avatar: await conn.getProfilePicture(m.sender).catch(_ => ''),
+    avatar: await conn.profilePictureUrl(conn.user.jid).catch(_ => './src/avatar_contact.png'),
     comment: text,
     username: conn.getName(m.sender)
   }), 'yt-comment.png', 'Here is your comment', m)
