ffd5c7b742103e6f6b0b9567eec4c92a2126bb9d
rydev99 <100022705+rydev99@users.noreply.github.com>
2022-03-29 03:04:13 +0700

    update autorespon.js

--- plugins/autorespon.js
+++ plugins/autorespon.js
@@ -3,14 +3,13 @@ let handler = m => m
 
 handler.all = async function (m, { conn }) {
 let set = db.data.settings[this.user.jid]
-if (set.autoupdatestatus) {
+
         if (new Date() * 1 - set.status > 1000) {
-            let _uptime = process.uptime() * 1000
-            let uptime = conn.clockString(_uptime)
+      
             await m.reply("oke")
             set.status = new Date() * 1
         }
-    }
+    
  
  }
 export default handler
