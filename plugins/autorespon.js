cb26fdadc348c3d220f58b2d66e3e9892d2f3146
rydev99 <100022705+rydev99@users.noreply.github.com>
2022-03-29 03:10:12 +0700

    update autorespon.js

--- plugins/autorespon.js
+++ plugins/autorespon.js
@@ -1,15 +1,8 @@
-import fs from 'fs'
-let handler = m => m
 
-handler.all = async function (m, { conn }) {
-let set = db.data.settings[this.user.jid]
-
-        if (new Date() * 1 - set.status > 1000) {
-      
+let handler = async (m, { conn }) => {
+        if (new Date() * 1 - status > 1000) {
             await m.reply("oke")
-            set.status = new Date() * 1
+            status = new Date() * 1
         }
-    
- 
  }
 export default handler
