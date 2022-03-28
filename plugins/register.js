fb9245d2e460c0e2ba1a3a5228d3511598c31923
rydev99 <100022705+rydev99@users.noreply.github.com>
2022-03-29 01:40:15 +0700

    update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -1,8 +1,7 @@
-import createHash from 'crypto'
+import { createHash } from 'crypto'
 let Reg = /(.*)([.|])([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix }) {
 let user = global.db.data.users[m.sender]
-  m.reply(user)
   if (user.registered === true) throw `Anda sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
   if (!Reg.test(text)) throw `Foemat salah\n*${usedPrefix}daftar <nama>.umur>*`
   let [_, name, splitter, age] = text.match(Reg)
