08060dfd0628c0373f5d2192b738af1104341e23
rydev99 <100022705+rydev99@users.noreply.github.com>
2022-03-29 02:06:12 +0700

    update _sendDb.js

--- plugins/_sendDb.js
+++ plugins/_sendDb.js
@@ -1,6 +1,10 @@
+import fetch from 'node-fetch'
 let handler = async(m, { conn, text }) => {
-
-conn.sendFile(m.chat, "https://json-server.rynz01.repl.co/", "db.json", null, m)
+let res = fetch("https://json-server.rynz01.repl.co/")
+if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if(!json.result[0]) throw json
+m.reply(json)
 }
 
 handler.command = /^('db')$/i
