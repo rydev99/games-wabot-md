0b8b5118ce987b500582f0f3e8a10bfb0ece7cb8
rydev99 <100022705+rydev99@users.noreply.github.com>
2022-03-29 10:03:54 +0700

    update stickermeme.js

---
+++ plugins/stickermeme.js
@@ -0,0 +1,25 @@
+import { sticker } from '../lib/sticker.js'
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+import { webp2png } from '../lib/webp2mp4.js'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+    let [atas, bawah] = text.split`|`
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (!mime) throw `balas gambar dengan perintah\n\n${usedPrefix + command} <${atas ? atas : 'teks atas'}>|<${bawah ? bawah : 'teks bawah'}>`
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
+    let img = await q.download()
+    let url = await uploadImage(img)
+    meme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas ? atas : '')}/${encodeURIComponent(bawah ? bawah : '')}.png?background=${url}`
+    stiker = await sticker(false, meme, global.packname, global.author)
+    if (stiker) await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+
+}
+handler.help = ['stikermeme <teks atas>|<teks bawah>']
+handler.tags = ['sticker']
+handler.command = /^(s(tic?ker)?meme)$/i
+
+handler.limit = true
+
+export default handler
